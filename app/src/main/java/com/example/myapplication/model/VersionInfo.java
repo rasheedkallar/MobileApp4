100063aa3362d1883e9250d5adf7a9bd0cf436a7
Abdul Rasheed Aboobacker <rasheedkallar@hotmail.com>
2026-09-09 08:03:05 +0400

    Auto Install implimented

---
+++ app/src/main/java/com/example/myapplication/model/VersionInfo.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.model;
+
+public class VersionInfo {
+    public long fileSize;
+    public String versionName;
+    public String sha256;
+    public int versionCode;
+    public String platform;
+    public String apkUrl;
+    public String notes;
+}
